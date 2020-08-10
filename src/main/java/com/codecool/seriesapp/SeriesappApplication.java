b5bf1439c33a1cd99d21367d410de74d25c46eb3
Alex Szabo <szabbo87@gmail.com>
2020-08-10 13:04:51 +0200

    base

---
+++ src/main/java/com/codecool/seriesapp/SeriesappApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeriesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeriesappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/seriesapp/SeriesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.seriesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
