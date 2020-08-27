9c12ea4d77261db13381e76f18922028addb09b5
Horvath Csaba <csaba0411@gmail.com>
2020-08-27 15:28:25 +0200

    tests bugfix

--- src/test/java/com/codecool/seriesapp/repository/AllRepositoryTest.java
+++ src/test/java/com/codecool/seriesapp/repository/AllRepositoryTest.java
@@ -4,11 +4,14 @@ import com.codecool.seriesapp.model.entity.FavouriteSeries;
 import com.codecool.seriesapp.model.entity.Member;
 import com.codecool.seriesapp.model.entity.VotedSeries;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.time.LocalDate;
 import java.util.List;
@@ -31,15 +34,6 @@ public class AllRepositoryTest {
 
 
     @Test
-    public void saveFavouriteSeries() {
-        FavouriteSeries series = FavouriteSeries.builder()
-                .showId(11)
-                .build();
-        favouriteSeriesRepository.save(series);
-        assertThat(favouriteSeriesRepository.findAll()).hasSize(1);
-    }
-
-    @Test
     public void saveMember() {
         Member csaba = Member.builder()
                 .birthDate(LocalDate.of(2000, 6, 15))
@@ -48,8 +42,17 @@ public class AllRepositoryTest {
                 .build();
         memberRepository.save(csaba);
         assertThat(memberRepository.findAll()).hasSize(1);
+        memberRepository.delete(csaba);
+    }
 
-
+    @Test
+    public void saveFavouriteSeries() {
+        FavouriteSeries series = FavouriteSeries.builder()
+                .showId(11)
+                .build();
+        favouriteSeriesRepository.save(series);
+        assertThat(favouriteSeriesRepository.findAll()).hasSize(1);
+        favouriteSeriesRepository.delete(series);
     }
 
     @Test
@@ -63,11 +66,12 @@ public class AllRepositoryTest {
                 .series(teszt)
                 .build();
 
-        teszt.setMember(alex);
+//        teszt.setMember(alex);
         memberRepository.save(alex);
 
         List<Member> members = memberRepository.findAll();
         assertThat(members).hasSize(1);
+        memberRepository.delete(alex);
     }
 
     @Test
@@ -75,8 +79,8 @@ public class AllRepositoryTest {
         VotedSeries votedSeries = VotedSeries.builder().showId(5).seriesRating(9.1).build();
         votedSeriesRepository.save(votedSeries);
         assertThat(votedSeriesRepository.findAll()).hasSize(1);
-    }
 
+    }
 
     @Test
     public void setVotedSeriesRating() {
@@ -87,4 +91,42 @@ public class AllRepositoryTest {
 
     }
 
+    @Test
+    public void saveUniqueFieldTwice() {
+        FavouriteSeries series1 = FavouriteSeries.builder()
+                .showId(10)
+                .build();
+
+        favouriteSeriesRepository.saveAndFlush(series1);
+        FavouriteSeries series2 = FavouriteSeries.builder()
+                .showId(10)
+                .build();
+
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            favouriteSeriesRepository.saveAndFlush(series2);
+        });
+        favouriteSeriesRepository.delete(series1);
+        favouriteSeriesRepository.delete(series2);
+    }
+
+    @Test
+    public void memberIsPeristedWithFavouriteSeries() {
+        FavouriteSeries fav = FavouriteSeries.builder()
+                .showId(100)
+                .build();
+
+        Member member = Member.builder()
+                .username("Joci")
+                .series(fav)
+                .build();
+
+        memberRepository.saveAndFlush(member);
+
+        List<FavouriteSeries> favs = favouriteSeriesRepository.findAll();
+        assertThat(favs)
+                .hasSize(1)
+                .allMatch(fav1 -> fav1.getId() > 0L);
+        memberRepository.delete(member);
+        favouriteSeriesRepository.delete(fav);
+    }
 }
