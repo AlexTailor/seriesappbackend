2a959f2c7bcd70720a360bc1bfaca20c9b9aaacb
Alex Szabo <szabbo87@gmail.com>
2020-08-27 13:59:24 +0200

    test: unit tests

--- src/test/java/com/codecool/seriesapp/repository/AllRepositoryTest.java
+++ src/test/java/com/codecool/seriesapp/repository/AllRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,8 +26,34 @@ public class AllRepositoryTest {
     @Autowired
     VotedSeriesRepository votedSeriesRepository;
 
+    @Autowired
+    FavouriteSeriesRepository favouriteSeriesRepository;
+
+
     @Test
-    public void saveUserWithFavouriteSeries() {
+    public void saveFavouriteSeries() {
+        FavouriteSeries series = FavouriteSeries.builder()
+                .showId(11)
+                .build();
+        favouriteSeriesRepository.save(series);
+        assertThat(favouriteSeriesRepository.findAll()).hasSize(1);
+    }
+
+    @Test
+    public void saveMember() {
+        Member csaba = Member.builder()
+                .birthDate(LocalDate.of(2000, 6, 15))
+                .email("szabbo87@gmail.com")
+                .username("Csaba")
+                .build();
+        memberRepository.save(csaba);
+        assertThat(memberRepository.findAll()).hasSize(1);
+
+
+    }
+
+    @Test
+    public void saveMemberWithFavouriteSeries() {
         FavouriteSeries teszt = FavouriteSeries.builder()
                 .showId(5)
                 .build();
@@ -44,16 +71,19 @@ public class AllRepositoryTest {
     }
 
     @Test
-    public void saveVotedSeriesRating() {
-
-        VotedSeries votedSeries = VotedSeries.builder()
-                .showId(1)
-                .seriesRating(7.0)
-                .build();
+    public void saveVotedSeries() {
+        VotedSeries votedSeries = VotedSeries.builder().showId(5).seriesRating(9.1).build();
         votedSeriesRepository.save(votedSeries);
-        votedSeriesRepository.setSeriesRating(1, 7.1);
-        System.out.println(votedSeries.getSeriesRating());
-        assertThat(votedSeries.getSeriesRating()).isEqualTo(7.1);
+        assertThat(votedSeriesRepository.findAll()).hasSize(1);
+    }
+
+
+    @Test
+    public void setVotedSeriesRating() {
+        VotedSeries votedSeries = VotedSeries.builder().showId(1).seriesRating(7.0).build();
+        votedSeriesRepository.save(votedSeries);
+        votedSeriesRepository.setSeriesRating(1, 0.1);
+        assertThat(votedSeriesRepository.getSeriesRatingByShowId(1)).isEqualTo(7.1);
 
     }
 
